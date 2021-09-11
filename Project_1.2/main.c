4d1f955f979e5fb1d9cb0edac26dfdc7f8810220
8268826 <86241573+8268826@users.noreply.github.com>
2021-09-11 09:30:56 +0300

    Update main.c

--- Project_1.2/main.c
+++ Project_1.2/main.c
@@ -7,15 +7,15 @@ int main()
     printf("Input a= "); scanf("%d",&a);
     printf("Input b= "); scanf("%d",&b);
     printf("%d + %d = %d\n",a,b,a+b);
-    printf("%d + %d = %d\n",a,b,a-b);
-    printf("%d + %d = %d\n",a,b,a*b);
+    printf("%d - %d = %d\n",a,b,a-b);
+    printf("%d * %d = %d\n",a,b,a*b);
     if(b!=0)
     {
-        printf("%d + %d = %.2f\n",a,b,(float)a/b);
+        printf("%d / %d = %.2f\n",a,b,(float)a/b);
     }
     else
     {
-        printf("%d + %d, Impossible to divide by 0",a,b);
+        printf("%d / %d, Impossible to divide by 0",a,b);
     }
     return 0;
 }
