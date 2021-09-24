7ce2e851fa862d988a8d03212ae382299ff39243
8268826 <86241573+8268826@users.noreply.github.com>
2021-09-24 20:59:19 +0300

    Update main.cpp

--- Project_3.3/main.cpp
+++ Project_3.3/main.cpp
@@ -4,21 +4,28 @@ using namespace std;
 
 int main()
 {
-    char c;
+    string c;
     fstream input,output;
     input.open("text.txt",ios::in);
     if(!input)
     {
-        cout<<"Can not open this file!"<<endl;
+        cout<<"Can not open \"text.txt\"!"<<endl;
+    }
+    else
+    {
+        cout<<"\"text.txt\" is opened!"<<endl;
     }
     output.open("textCopy.txt",ios::out);
+    cout<<"\"textCopy.txt\" is opened!"<<endl;
     while(!input.eof())
     {
         input>>c;
         output<<c;
+        output<<' ';
     }
-    cout<<"File is copy"<<endl;
+    cout<<"Data has been copied from \"text.txt\" to \"textCopy.txt\"!"<<endl;
     input.close();
     output.close();
+    cout<<"File is closed!"<<endl;
     return 0;
 }
