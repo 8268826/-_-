1657c584ec900017e5ef83ae4505a4b56655f45d
8268826 <86241573+8268826@users.noreply.github.com>
2021-09-24 20:48:28 +0300

    Add files via upload

---
+++ Project_3.2/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float s,n,m,r,p;
+    cout<<"Input S,n,m: "<<endl;
+    cin>>s>>n>>m;
+    for(int i=1;i<10000;i++)
+    {
+        p=i/1000;
+        r=p/100;
+        float m1=(s*r*pow(1+r,n))/12*(pow(1+r,n)-1);
+        if(m1>m)
+        {
+            cout<<"p= "<<p<<endl;
+        }
+    }
+
+    return 0;
+}
