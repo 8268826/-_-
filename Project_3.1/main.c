61fefd6aedf1f8c4bc5f169876606faf5cb9d5ad
8268826 <86241573+8268826@users.noreply.github.com>
2021-10-01 20:49:35 +0300

    Update main.c

--- Project_3.1/main.c
+++ Project_3.1/main.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
     float s,n,p,r,m;
-    printf("Input S,p,n:\n"); scanf("%f%f%f",&s,&p,&n);
+    cout<<"Input S,p,n: "<<endl; cin>>s>>p>>n;
     r=p/100;
     m = (s * r * pow(1 + r, n)) / (12 * (pow(1 + r, n) - 1));
-    printf("m= %g",m);
+    cout<<"m= "<<m;
     return 0;
 }
