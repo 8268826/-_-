8e781b7c46553a07ee233d7402f4da5194708bb2
8268826 <86241573+8268826@users.noreply.github.com>
2021-10-09 11:12:55 +0300

    Update main.cpp

--- Project_4.4/main.cpp
+++ Project_4.4/main.cpp
@@ -5,10 +5,16 @@ int main()
 {
 	for (int i = 0; i < 13; i++)
 	{
-		for (int j = 0; j < 45; j++)
+		for (int j = 0; j < 25; j++)
 		{
-			if (i <= 5 && j <= 7) cout << "*";
-			else cout << "-";
+			if (i <= 5 && i%2==0 && j <= 7) cout << "* ";
+			else if(i<=5 && i%2!=0 && j<=7)cout <<" *";
+			else if(i%2==0)
+            {
+                cout << "||";
+            }
+            else
+                cout<<" ";
 		}
 		cout << endl;
 	}
