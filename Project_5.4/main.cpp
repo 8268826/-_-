2bfa3d6ca94a9e86e6b429d4040a2a9d5fbe2fe2
8268826 <86241573+8268826@users.noreply.github.com>
2021-10-16 09:56:22 +0300

    Update main.cpp

--- Project_5.4/main.cpp
+++ Project_5.4/main.cpp
@@ -3,74 +3,75 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL,"Rus");
-    int m,n,select;
-    cout<<"Введите количество столбцов: "; cin>>m;
-    cout<<"Введите количество строк: "; cin>>n;
+    setlocale(LC_ALL, "Rus");
+    int m, n, select;
+    cout << "Введите количество столбцов: "; cin >> m;
+    cout << "Введите количество строк: "; cin >> n;
     int** a = new int* [m];
-	for (int i = 0; i < m; i++)
-	{
-		a[i] = new int[n];
-	}
-	Again:
-	cout<<"Enter the matrix: "<<endl;
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-		    cout<<"a["<<i<<"]["<<j<<"]= ";
-			cin >>a[i][j];
-		}
-	}
-	do{
-            system("cls");
-        cout<<"1.Re-enter the matrix"<<endl;
-        cout<<"45).Написать программу, которая вводит по строкам с клавиатуры двумерный массив и вычисляет суммы его элементов по столбцам."<<endl;
-        cout<<"47).Написать программу, которая вычисляет сумму диагональных элементов квадратной матрицы."<<endl;
-        cout<<"Пожалуйста, выбери: "; cin>>select;
-        switch(select)
+    for (int i = 0; i < m; i++)
+    {
+        a[i] = new int[n];
+    }
+Again:
+    cout << "Enter the matrix: " << endl;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
         {
-            default:{cout<<"Пожалуйста, введите еще раз!"<<endl; system("pause"); break;}
-            case 0: break;
-            case 1:
-                    goto Again;
-            case 45: {
-                int sum;
+            cout << "a[" << i << "][" << j << "]= ";
+            cin >> a[i][j];
+        }
+    }
+    do {
+        system("cls");
+        cout << "1.Re-enter the matrix" << endl;
+        cout << "45).Написать программу, которая вводит по строкам с клавиатуры двумерный массив и вычисляет суммы его элементов по столбцам." << endl;
+        cout << "47).Написать программу, которая вычисляет сумму диагональных элементов квадратной матрицы." << endl;
+        cout << "Пожалуйста, выбери: "; cin >> select;
+        switch (select)
+        {
+        default: {cout << "Пожалуйста, введите еще раз!" << endl; system("pause"); break; }
+        case 0: break;
+        case 1:
+            goto Again;
+        case 45: {
+            int sum=0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    sum += a[j][i];
+
+                }
+                cout << "a[" << i << "]= " << sum << endl;
+                sum = 0;
+            }
+            system("pause");
+            break;
+        }
+        case 47:
+        {
+            int sum=0;
+            if (m != n)
+            {
+                cout << "Не квадратная матрица!" << endl;
+            }
+            else {
                 for (int i = 0; i < n; i++)
                 {
-                    for (int j = 0; j < m; j++)
-                    {
-                        sum += a[j][i];
-                    }
-                    cout << "a[" << j << "]= " << sum << endl;
-                    sum = 0;
+                    sum += a[i][i];
                 }
-                system("pause");
-                break;
+                cout << sum<<endl;
             }
-            case 47:
-                {
-                    int sum;
-                    if(m!=n)
-                    {
-                        cout<<"Не квадратная матрица!"<<endl;
-                    }
-                    else{
-                         for (int i = 0; i < n; i++)
-                    {
-                        sum += a[i][i];
-                    }
-                    cout<<sum;
-                    }
-                    system("pause");
-                    break;
-                }
+            system("pause");
+            break;
         }
-	}while(select);
+        }
+    } while (select);
 
-	for (int i = 0; i < m; i++)
-	{
-		delete[] a[i];
-	}
-	delete[] a;
+    for (int i = 0; i < m; i++)
+    {
+        delete[] a[i];
+    }
+    delete[] a;
 }
