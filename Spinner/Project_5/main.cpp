25e759223938d5380508fa753d0adae02888d2a7
8268826 <86241573+8268826@users.noreply.github.com>
2021-10-30 14:01:50 +0300

    Add files via upload

---
+++ Spinner/Project_5/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n, k, demmax;
+Again:
+	cout << "Input (n,k): "; cin >> n >> k;
+	demmax = n;
+	if (n < 1 || k < 1)
+	{
+		cout << "Input is falsed!" << endl;
+		system("pause");
+		system("cls");
+		goto Again;
+	}
+	int* a = new int[n + 1];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = 0;
+	}
+	a[n / 2] = 1;
+	a[n] = 1;
+	while (k > 1)
+	{
+		int dem = 0, q;
+		demmax = 0;
+		for (int i = 0; i <= n; i++)
+		{
+			if (a[i] == 0)
+			{
+				dem++;
+			}
+			if (dem >= demmax && a[i] == 1)
+			{
+				demmax = dem;
+				dem = 0;
+				q = i;
+			}
+		}
+		q--;
+		a[q - demmax / 2] = 1;
+		k--;
+	}
+	cout << "Output: " << rintf(((float)demmax / 2) - 0.6) << " " << rintf(((float)demmax / 2));
+	delete[]a;
+	return 0;
+}
