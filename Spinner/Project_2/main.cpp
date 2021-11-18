5864e0ce650c707a3f1a32e08634b3e1cfd27c12
8268826 <86241573+8268826@users.noreply.github.com>
2021-11-18 18:13:13 +0300

    Update main.cpp

--- Spinner/Project_2/main.cpp
+++ Spinner/Project_2/main.cpp
@@ -1,20 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+bool check(float n)
+{
+	if (n <= 0 || n > 2e9 || n != rintf(n))
+		return false;
+	return true;
+}
+
 int main()
 {
-    int a;
-    bool b = false;
-    cout<<"Input: "; cin>>a;
-    cout<<"Output: "<<endl;
-    for (int i = 0; i < a; i++)
-        for (int j = 0; j < a; j++)
-            if (i * 3 + 4 * j == a)
-            {
-                b = true;
-                cout<< i << " " << j << endl;
-                break;
-            }
-    if (!b)
-        cout << 0 << endl << 0;
-        return 0;
+	float a;
+	bool b = false;
+	cout << "Input: ";
+Again:
+	cin >> a;
+	if (!check(a))
+	{
+		cout << "Input is falsed!" << endl;
+		system("pause");
+		system("cls");
+		cout << "Re-input: ";
+		goto Again;
+	}
+	cout << "Output: " << endl;
+	for (int i = 0; i < a; i++)
+		for (int j = 0; j < a; j++)
+			if (i * 3 + 4 * j == a)
+			{
+				b = true;
+				cout << i << " " << j << endl;
+				return 0;
+			}
+	if (!b)
+		cout << 0 << endl << 0;
+	return 0;
 }
