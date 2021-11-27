27ba120c5b49cc26637e21b8e2a31219f2901037
8268826 <86241573+8268826@users.noreply.github.com>
2021-11-27 03:40:36 +0300

    Update main.cpp

--- Spinner/Project_5/main.cpp
+++ Spinner/Project_5/main.cpp
@@ -1,49 +1,34 @@
 #include <iostream>
-#include <math.h>
+#include <deque>
+#include <algorithm>
+#define MAX 1e18
 using namespace std;
 
 int main()
 {
-	int n, k, demmax;
+	deque<int> tuan;
+	int n, k;
+	cout << "Input (n,k): ";
 Again:
-	cout << "Input (n,k): "; cin >> n >> k;
-	demmax = n;
-	if (n < 1 || k < 1 || n < k)
+	cin >> n >> k;
+	if (n < 1 || k < 1 || MAX < n < k )
 	{
 		cout << "Input is falsed!" << endl;
 		system("pause");
 		system("cls");
+		cout << "Re-input (n,k): ";
 		goto Again;
 	}
-	int* a = new int[n + 1];
-	for (int i = 0; i < n; i++)
+	tuan.push_back(n);
+	while (k > 0)
 	{
-		a[i] = 0;
-	}
-	a[n / 2] = 1;
-	a[n] = 1;
-	while (k > 1)
-	{
-		int dem = 0, q;
-		demmax = 0;
-		for (int i = 0; i <= n; i++)
-		{
-			if (a[i] == 0)
-				dem++;
-			else if (dem >= demmax && a[i] ==1)
-			{
-				demmax = dem;
-				dem = 0;
-				q = i;
-			}
-			else dem = 0;
-			
-		}
-		q--;
-		a[q - demmax / 2] = 1;
+		tuan.push_back(n / 2);
+		tuan.push_front((n - 1) / 2);
+		for (int i = 0; i < tuan.size(); i++) if (tuan[i] == *max_element(tuan.begin(), tuan.end()))
+			tuan.erase(tuan.begin() + i);
+		n = *max_element(tuan.begin(), tuan.end());
 		k--;
 	}
-	cout << "Output: " << rintf(((float)demmax / 2) - 0.6) << " " << rintf(((float)demmax / 2));
-	delete[]a;
+	cout << tuan.front() << " " << tuan.back();
 	return 0;
 }
